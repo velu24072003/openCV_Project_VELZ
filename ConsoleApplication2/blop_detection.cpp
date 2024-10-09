5d872471672a9b877086d0cecb12cd0585c76765
velmurugan m <velu24072003@gmail.com>
2024-10-09 15:41:59 +0530

    Revert "updated"
    This reverts commit bbfcce16b58ce6c096987d66c059bfcb530958a6.

--- ConsoleApplication2/blop_detection.cpp
+++ ConsoleApplication2/blop_detection.cpp
@@ -1,44 +1,42 @@
-#include <opencv2/opencv.hpp>
-#include <opencv2/features2d.hpp>
-using namespace std;
-using namespace cv;
-
-int main() {
-    // Read image
-    cv::Mat im = cv::imread("david-clode-Oq9JrJb6K7M-unsplash1.jpg",0);
-    Mat sobelx, sobely, sobelxy;
-    Mat img_blur;
-    GaussianBlur(im, img_blur,Size(3,3),0,0);
-    Sobel(img_blur, sobelx, CV_64F, 1, 0, 5);
-    Sobel(img_blur, sobely, CV_64F, 0, 1, 5);
-    Sobel(img_blur, sobelxy, CV_64F, 1, 1, 5);
-   
-    //// Display Sobel edge detection images
-    imshow("Sobel X", sobelx);
-    waitKey(0);
-    imshow("Sobel Y", sobely);
-    waitKey(0);
-    imshow("Sobel XY using Sobel() function", sobelxy);
-    waitKey(0);
-
-    // Set up the detector with default parameters
-    cv::Ptr<cv::SimpleBlobDetector> detector = cv::SimpleBlobDetector::create();
-    // Detect blobs
-    std::vector<cv::KeyPoint> keypoints;
-    detector->detect(im, keypoints);
-
-    //// Draw detected blobs as red circles
-    cv::Mat im_with_keypoints;
-    cv::drawKeypoints(im, keypoints, im_with_keypoints, cv::Scalar(0, 0, 255), cv::DrawMatchesFlags::DEFAULT);
-
-    //// Show blobs
-    cv::imshow("Keypoints", im_with_keypoints);
-    cv::waitKey(0);
-     //Canny edge detection
-    Mat edges;
-    Canny(img_blur, edges, 100, 200, 3, false);
-    // Display canny edge detected image
-    imshow("Canny edge detection", edges);
-    waitKey(0);
-    return 0;
-}
+//#include <opencv2/opencv.hpp>
+//#include <opencv2/features2d.hpp>
+//using namespace std;
+//using namespace cv;
+//
+//int main() {
+//    // Read image
+//    cv::Mat im = cv::imread("C:\\Users\\velu2\\Downloads\\david-clode-Oq9JrJb6K7M-unsplash1.jpg",0);
+//    Mat sobelx, sobely, sobelxy;
+//    Mat img_blur;
+//    GaussianBlur(im, img_blur,Size(3,3),0,0);
+//    Sobel(img_blur, sobelx, CV_64F, 1, 0, 5);
+//   
+//    //// Display Sobel edge detection images
+//    imshow("Sobel X", sobelx);
+//    waitKey(0);
+//    imshow("Sobel Y", sobely);
+//    waitKey(0);
+//    imshow("Sobel XY using Sobel() function", sobelxy);
+//    waitKey(0);
+//
+//    // Set up the detector with default parameters
+//    cv::Ptr<cv::SimpleBlobDetector> detector = cv::SimpleBlobDetector::create();
+//    // Detect blobs
+//    std::vector<cv::KeyPoint> keypoints;
+//    detector->detect(im, keypoints);
+//
+//    //// Draw detected blobs as red circles
+//    cv::Mat im_with_keypoints;
+//    cv::drawKeypoints(im, keypoints, im_with_keypoints, cv::Scalar(0, 0, 255), cv::DrawMatchesFlags::DEFAULT);
+//
+//    //// Show blobs
+//    cv::imshow("Keypoints", im_with_keypoints);
+//    cv::waitKey(0);
+//     //Canny edge detection
+//    Mat edges;
+//    Canny(img_blur, edges, 100, 200, 3, false);
+//    // Display canny edge detected image
+//    imshow("Canny edge detection", edges);
+//    waitKey(0);
+//    return 0;
+//}
--- ConsoleApplication2/task3.cpp
+++ ConsoleApplication2/task3.cpp
@@ -77,15 +77,15 @@
 //        if (y == 8)
 //            return 0;
 //        k = to_string(y);
-//
-//<<<<<<< HEAD
-//        input = imread("C:\\Users\\velu2\\OneDrive\\Pictures\\Saved Pictures\\senior.jpg");/
-//=======
+<<<<<<< HEAD
+<<<<<<< HEAD
+//        input = imread("C:\\Users\\velu2\\OneDrive\\Pictures\\Saved Pictures\\senior.jpg");
+=======
 //        input = imread("C:\\Users\\velu2\\OneDrive\\Pictures\\Saved Pictures\\IMG-20240928-WA000" + k + ".jpg");
-//>>>>>>> ab1959f508e076a4aebba21e383af7a171f46665
-//=======
+>>>>>>> ab1959f508e076a4aebba21e383af7a171f46665
+=======
 //        input = imread("C:\\Users\\velu2\\OneDrive\\Pictures\\Saved Pictures\\IMG-20240928-WA000" + k + ".jpg");
-//>>>>>>> ab1959f508e076a4aebba21e383af7a171f46665
+>>>>>>> ab1959f508e076a4aebba21e383af7a171f46665
 //        if (input.empty()) {
 //            continue;
 //        }
@@ -98,16 +98,16 @@
 //        Mat binary;
 //        Mat inrange;
 //        // inrange function to isolate the cashew nut alone
-//<<<<<<< HEAD
-//<<<<<<< HEAD
+<<<<<<< HEAD
+<<<<<<< HEAD
 //        threshold(vv1, inrange, 140, 255, THRESH_BINARY);
 //       //inRange(input,Scalar(42,82,87), Scalar(255,255,255), inrange);
-//=======
+=======
 //        inRange(input,Scalar(42,82,87), Scalar(255,255,255), inrange);
-//>>>>>>> ab1959f508e076a4aebba21e383af7a171f46665
-//=======
+>>>>>>> ab1959f508e076a4aebba21e383af7a171f46665
+=======
 //        inRange(input,Scalar(42,82,87), Scalar(255,255,255), inrange);
-//>>>>>>> ab1959f508e076a4aebba21e383af7a171f46665
+>>>>>>> ab1959f508e076a4aebba21e383af7a171f46665
 //        imshow("usingInrange", inrange);
 //        waitKey(0);
 //        /*Athreshold(vv1, binary, 76, 255, THRESH_BINARY);
@@ -126,16 +126,16 @@
 //            }
 //
 //        }
-//<<<<<<< HEAD
-//<<<<<<< HEAD
-//=======
-//
-///
-//>>>>>>> ab1959f508e076a4aebba21e383af7a171f46665
-//=======
+<<<<<<< HEAD
+<<<<<<< HEAD
+=======
 //
 //
-//>>>>>>> ab1959f508e076a4aebba21e383af7a171f46665
+>>>>>>> ab1959f508e076a4aebba21e383af7a171f46665
+=======
+//
+//
+>>>>>>> ab1959f508e076a4aebba21e383af7a171f46665
 //        destroyAllWindows();
 //        y++;
 //    }
